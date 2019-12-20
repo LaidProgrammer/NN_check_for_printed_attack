614e68a7c4a4e47ca799ae107e9be4f4be88e2d0
LaidProgrammer <gadago@yandex.ru>
2019-12-20 23:08:00 +0300

    add net with my net and modify main.py which now working with MTCNN

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from PIL import Image
+import cv2
 from mtcnn import mtcnn
 import torch
-import cv2
+from torch import nn
+from torchvision import models
 from pathlib import Path
+
+
+class Linear(nn.Module):
+
+    def __init__(self, n_classes):
+        super().__init__()
+        self.ln1 = nn.Linear(n_classes, 2)
+
+    def forward(self, x):
+        x = self.ln1(x)
+        # x = self.ln6(x)
+        # преобразование в одномерный вектор
+        return x
+
+
+my_lin_net = Linear(512)
+my_net = models.resnet18()
+my_net.fc = my_lin_net
+
+my_net = torch.load(Path("net/net.pth"), map_location=torch.device("cpu"))
+my_net.eval()
 
 cap = cv2.VideoCapture(0)
 
 mt_nn = mtcnn.MTCNN(Path("mtcnn"))
 
-# mynet = torch.load("./net.pth")
-
-# mynet.eval()
-
 while True:
     ret, img = cap.read()
     cv2.imshow("camera", img)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     my_img = Image.fromarray(img)
 
@@ -23,8 +41,10 @@ while True:
     if len(bounds[0]) == 0:
         continue
     x, y, w, h, p = bounds[0][0]
-    print(x, y, w, h, p)
-    cv2.imshow("camera", cv2.rectangle(img, (int(x), int(y)), (int(w), int(h)), (255, 0, 0)))
+    cv2.imshow("camera", cv2.rectangle(img, (int(x), int(y)), (int(w), int(h)), (0, 255, 0)))
+    img_s = list()
+    img_s.append(img)
+    # print(my_net(torch.Tensor(img_s)))
     if cv2.waitKey(10) == 27:
         cv2.destroyAllWindows()
         break
