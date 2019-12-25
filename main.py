6663273a6d94a808feb4c91fbff7326e9f3be585
LaidProgrammer <gadago@yandex.ru>
2019-12-25 11:24:43 +0300

    add text, optimise searching, move to resnet50, increase linnear end to 2 layers

--- main.py
+++ main.py
@@ -13,18 +13,22 @@ class Linear(nn.Module):
 
     def __init__(self, n_classes):
         super().__init__()
-        self.ln1 = nn.Linear(n_classes, 2)
+        self.ln1 = nn.Linear(n_classes, 32)
+        self.ReLU = nn.ReLU()
+        self.ln2 = nn.Linear(32, 2)
 
     def forward(self, x):
         x = self.ln1(x)
+        x = self.ReLU(x)
+        x = self.ln2(x)
         return x
 
 
-my_lin_net = Linear(512)
-my_net = models.resnet18()
+my_lin_net = Linear(2048)
+my_net = models.resnet50()
 my_net.fc = my_lin_net
 
-my_net.load_state_dict(torch.load(Path('./net/saved.pth'), map_location=torch.device("cpu")))
+my_net.load_state_dict(torch.load(Path('./net/resnet50.pth'), map_location=torch.device("cpu")))
 my_net.eval()
 
 transform = Compose([
@@ -36,9 +40,16 @@ cap = cv2.VideoCapture(0)
 
 mt_nn = mtcnn.MTCNN(Path("mtcnn"))
 
+count = 0
+
 while True:
     ret, img = cap.read()
     cv2.imshow("camera", img)
+
+    count += 1
+    count %= 5
+    if count == 0:
+        continue
 
     my_img = Image.fromarray(img)
     bounds = mt_nn.detect_faces(my_img)
@@ -48,15 +59,26 @@ while True:
     my_img = my_img.crop((int(x), int(y), int(x + w), int(y + h)))
     my_img = my_img.resize((196, 196))
     my_img = transform(my_img)
+
     test = my_net(my_img[None])
     test = nn.Softmax(dim=1)(test)
     test = test.detach().numpy()[0]
     test_out = np.argmax(test)
+
     color = (0, 0, 255)
-    # print(test)
+
+    text = "Fake"
+
     if test_out == 0:
         color = (0, 255, 0)
-    cv2.imshow("camera", cv2.rectangle(img, (int(x), int(y)), (int(w), int(h)), color))
+        text = "Real"
+    cv2.imshow("camera", cv2.putText(cv2.rectangle(img, (int(x), int(y)), (int(w), int(h)), color),
+                                     text=text,
+                                     org=(int(x), int(y)),
+                                     fontFace=cv2.FONT_HERSHEY_COMPLEX,
+                                     fontScale=1,
+                                     color=color,
+                                     thickness=1))
 
     if cv2.waitKey(10) == 27:
         cv2.destroyAllWindows()
